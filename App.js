ec4672f766e4012953d66f2c610aba951e341507
Senad Djerlek <senajerlek@gmail.com>
2023-12-25 06:02:36 +0100

    na main

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Countries from "./components/Countries";
-
 export default function App() {
   return (
     <View style={styles.container}>
